fe3e2bd6529507d5bb07c041f720e054e99f8fd9
dmytrosaprykin77 <rv.dmytro@gmail.com>
2025-09-30 23:35:07 +0300

    5

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-console.clear()
+
 const root = document.querySelector('root')
 const input = document.querySelector('input');
  const btn = document.querySelector('button');
 let cod = document.querySelector('textarea')
 let help2 = document.querySelector('help2')
+let levelbox = document.querySelector('.level-box')
 btn.addEventListener('click', () => {
   eval(cod.value)
  });
@@ -14,67 +15,127 @@ const help1 = document.querySelector(".help1");
   
    let arrow = document.querySelector(".arrow");
     arrow.src = "none.png";
-
-
-  let button = document.querySelector("button")
-  button.addEventListener('click',()=>{
-
-  })
-
-  //async function arrowflay() {
-  // for(let i = 0; i < 750; i++){
-  //x += 2;
-  
-   //await new Promise(r => setTimeout(r, 0.001));
-   
-   //arrow.style.left = x + "px";
+  // async function arrowflay() {
+  //  for(let i = 0; i < 750; i++){
+   //  x += 2;
+   // await new Promise(r => setTimeout(r, 0.001));
+    //arrow.style.left = x + "px";
        // }
  // }
  // arrowflay();
-
   let index = 0;
+  let l = 1;
   let isTyping = false;
   let interval = null;
   let currentText = "";
   let charIndex = 0;
-
- 
-
- 
+  let usedCommands = new Set();
   let pointsDisplay = document.querySelector('.points-box')
   let points = 0;
-  pointsDisplay.textContent = "POINTS: " + points;
+
+
 
   const commands = [
-  `let arrow = document.querySelector(".arrow");`,
-
+  `let arrow = document.querySelector(".arrow");`];
+const commands2 = [
   `let arrow = document.querySelector(".arrow");
-  async function arrowflay() {
-for(let i = 0; i < 850; i++) {
-  x += 2;
-  await new Promise(r => setTimeout(r, 0.001));
-  arrow.style.left = x + "px";
-}
-}`,
-
-  `arrowflay();`
-];
- 
+  function arrowfly() {
+        for(let i = 0; i < 750; i++) {
+            x += 2;
+        arrow.style.left = x + "px" }}`];
+        let commands3 = [`let arrow = document.querySelector(".arrow");
+          async function arrowfly() {
+    for(let i = 0; i < 750; i++){
+     x += 2;
+    await new Promise(r => setTimeout(r,0.001));
+    arrow.style.left = x + "px";
+        }}
+    arrowfly();`]
    let dialogue2 = document.querySelector(".dial-text-lvl1");
   btn.addEventListener('click', () => {
-    let userInput = cod.value
-
-    if(commands.includes(userInput)){
-      points += 33.3;
+   
+if (points>0  && commands.includes(cod.value) ){
+alert(`Ви вже використовували цю команду!`)
+    }
+    if (points == 0 && commands.includes(cod.value)){
+  points += 33.3;
       alert('Правильна команда! +33.3 поінта')
-      arrow.src = "pixil-gif-drawing (2).gif";
       emotion.src = "happy.png"
-    } else {
+      levelbox.textContent = "LEVEL: 2"
+      arrow.src = "pixil-gif-drawing (2).gif";
+      l = 2;
+      console.log(l);
+
+     if (points == 0 && !commands.includes(cod.value)) {
       alert('Неправильна команда')
       emotion.src = "sad.png"
+     }
+pointsDisplay.textContent = "POINTS: " + points;
+
+   
+  }
+
+})
+btn.addEventListener('click', () => {
+   
+if (points == 66.6  && commands2.includes(cod.value) ){
+alert(`Ви вже використовували цю команду!`)
     }
-    pointsDisplay.textContent = "POINTS: " + points;
-  })
+    if (points == 33.3 && commands2.includes(cod.value)){
+  points += 33.3;
+      alert('Правильна команда! +33.3 поінта')
+     
+      emotion.src = "happy.png"
+      levelbox.textContent = "LEVEL: 3"
+      l = 3;
+      console.log(l);
+
+     if (points == 33.3 && !commands2.includes(cod.value)) {
+      alert('Неправильна команда')
+      emotion.src = "sad.png"
+     }
+pointsDisplay.textContent = "POINTS: " + points;
+
+   
+  }
+
+})
+btn.addEventListener('click', () => {
+  if (points == 66.6 && commands3.includes(cod.value)){
+  points += 33.3;
+  l = 4;
+  console.log(l);
+      alert('Правильна команда! +33.3 поінта! Це все, дякую що пограли! :)')
+      appledelite()
+     levelbox.textContent = "VICTORY!"
+      emotion.src = "happy.png"
+     if (points == 66.6 && !commands3.includes(cod.value)) {
+      alert('Неправильна команда')
+      
+      emotion.src = "sad.png"
+      
+     }
+pointsDisplay.textContent = "POINTS: " + points;
+
+   
+  }
+
+})
+
+
+
+console.log(l);
+
+   // if(commands.includes(userInput)){
+   //    points += 33.3;
+   //    alert('Правильна команда! +33.3 поінта')
+   //    arrow.src = "pixil-gif-drawing (2).gif";
+   //    emotion.src = "happy.png"
+   //  } else {
+     //alert('Неправильна команда')
+     // emotion.src = "sad.png"
+    //}
+  
   let box = document.querySelector(".dialogue1");
   let emotion = document.querySelector(".emotion1")
  async function changeimg() {
@@ -92,17 +153,28 @@ for(let i = 0; i < 850; i++) {
   let apple = document.querySelector(".apple")
   async function appledelite() 
 {
-  
+  await new Promise(r => setTimeout(r, 2900));
   apple.src = "pixil-gif-drawing (6).gif"
   await new Promise(r => setTimeout(r, 600));
   apple.src = "none.png"
+  arrow.src = "none.png"
 }
-if (arrow.style.left == "625px") {
-  appledelite();
-}
+
 console.log(arrow.style.left);
 
 const music = new Audio("Music fon.mp3");
   function checkCode(){ 
       let value = document.getElementById("answer").value.trim();
   }
+
+
+if (userCode === 'правильний код') {
+            correctSound.play("Win.mp3"); 
+        } 
+  else {
+        }
+
+
+
+ appledelite()
+
